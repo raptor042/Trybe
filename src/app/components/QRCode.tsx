fcbdd86273c4a2bc28b22c77820e4b1152b7d876
Benji Chimex <benjichimex@MacBooks-MacBook-Pro.local>
2024-06-30 00:50:46 +0100

    another commit

--- src/app/components/QRCode.tsx
+++ src/app/components/QRCode.tsx
@@ -1,13 +1,14 @@
 "use client"
 
-import React, { useRef } from 'react';
+import React from 'react';
 import { IoMdClose } from "react-icons/io";
 
 import * as htmlToImage from "html-to-image";
 import QRCode from "react-qr-code";
-import { IoDownload } from 'react-icons/io5';
+import { IoCopy, IoDownload } from 'react-icons/io5';
 import toast, { Toaster } from 'react-hot-toast';
 import download from "downloadjs";
+import Link from 'next/link';
 
 interface ModalProps {
     isOpen: boolean;
@@ -24,6 +25,8 @@ const QRCodeModal: React.FC<ModalProps> = ({ isOpen, onClose, url }) => {
         }
     };
 
+    let copied = false
+
     const downloadQRCode = () => {
         htmlToImage
           .toPng(document.getElementById("qr-code")!)
@@ -35,28 +38,40 @@ const QRCodeModal: React.FC<ModalProps> = ({ isOpen, onClose, url }) => {
 
             toast.error("Error downloading QR code.")
           });
-      };
+    };
+
+    const copyText = async () => {
+        await navigator.clipboard.writeText(url)
+        copied = true
+
+        setTimeout(() => {
+            copied = false
+        }, 2000);
+    }
 
     return (
-        <div
-            className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50"
-            onClick={handleOverlayClick}
-        >
-            <Toaster />
-            <button
-                className="absolute top-2 right-2 text-black  rounded-full p-2"
-                onClick={onClose}
-            >
-                <IoMdClose className="text-white text-2xl" />
-            </button>
-            <div id="qr-code" className="relative bg-[#19191B] p-4 rounded-lg m-2">
-                {url && <QRCode value={url} size={300} />}
-            </div>
-            <div className='flex justify-center m-2'>
-                <button onClick={downloadQRCode} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                    Download
-                    <IoDownload />
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50" onClick={handleOverlayClick}>
+            <div className="relative bg-[#19191B] p-4 rounded-lg">
+                <button className="absolute top-2 right-2 text-black rounded-full p-2" onClick={onClose}>
+                    <IoMdClose className="text-white" />
                 </button>
+                <div id="qr-code" className="relative bg-[#19191B] p-4 rounded-lg m-2">
+                    {url && <QRCode value={url} size={300} />}
+                </div>
+                <div className='flex items-center m-2'>
+                    <Link href={url} className='text-sm font-medium text-center text-white'>{url.slice(0, 12)}...</Link>
+                    <button onClick={copyText} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        {!copied && "Copy"}
+                        {!copied && <IoCopy />}
+                        {!copied && "Copied"}
+                    </button>
+                </div>
+                <div className='flex justify-center m-2'>
+                    <button onClick={downloadQRCode} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        Download
+                        <IoDownload />
+                    </button>
+                </div>
             </div>
         </div>
     );
