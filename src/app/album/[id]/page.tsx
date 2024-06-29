d014781e724625a146d3db9b804126c3851afdbc
Benji Chimex <benjichimex@MacBooks-MacBook-Pro.local>
2024-06-30 00:19:50 +0100

    another commit

--- src/app/album/[id]/page.tsx
+++ src/app/album/[id]/page.tsx
@@ -12,7 +12,7 @@ import { useParams, useRouter } from 'next/navigation';
 const Page = () => {
     const [album, setAlbum] = useState<string[]>([])
     const [joining, setJoining] = useState(false)
-    const [loading, setLoading] = useState(false)
+    const [loading, setLoading] = useState(true)
 
     const { address, isConnected } = useWeb3ModalAccount();
 
@@ -26,6 +26,7 @@ const Page = () => {
 
     const params = useParams();
     const id = params.id;
+    console.log(id)
 
     const router = useRouter()
 
@@ -43,10 +44,11 @@ const Page = () => {
         );
     
         try {
-            const album = await trybe.getAlbum(id)
+            const album = await trybe.getAlbum(Number(id))
             console.log(album)
     
             setAlbum(album)
+            setLoading(false)
         } catch (error) {
             console.log(error)
         }
@@ -92,28 +94,42 @@ const Page = () => {
     }
 
     return (
-        <div>
-            <main className='flex flex-col items-center w-full m-2'>
-                <Image src={"/album.svg"} alt='' width={300} height={200} />
-                <div className='flex flex-col gap-1 items-center'>
-                    <p className='font-bold text-2xl'>{isConnected ? `Album ${album[0]} -- ${album[4]}` : "Connect your wallet to join this album."}</p>
-                    {isConnected && <p className='text-sm'>{album[5]}</p>}
-                </div>
-            </main>
-
-            <button className='flex items-center p-1 rounded-2xl gap-3 px-5 bg-[#5773ff] m-2' onClick={joinAlbum}>
-                {!joining && <IoAdd />}
-                {!joining && <p>Join Album</p>}
-                {joining &&
-                    <div role="status">
-                        <svg aria-hidden="true" className="w-4 h-4 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
-                            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
-                        </svg>
-                        <span className="sr-only">Loading...</span>
+        <div className='flex flex-col items-center'>
+            {!loading &&
+                <main className='flex flex-col items-center w-full m-2'>
+                    <Image src={"/album.svg"} alt='' width={300} height={200} />
+                    <div className='flex flex-col gap-1 items-center'>
+                        <p className='font-bold text-2xl'>{`Album ${album[0]} - ${album[4]}`}</p>
+                        <p className='text-sm'>{album[5]}</p>
                     </div>
-                }
-            </button>
+                </main>
+            }
+
+            {loading &&
+                <div role="status" className="flex flex-col justify-center items-center m-4">
+                    <svg aria-hidden="true" className="w-16 h-16 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+                    </svg>
+                    <span className="sr-only">Loading...</span>
+                </div>
+            }
+
+            {!loading &&
+                <button className='flex items-center p-1 rounded-2xl gap-3 px-5 bg-[#5773ff] m-2' onClick={joinAlbum}>
+                    {!joining && <IoAdd />}
+                    {!joining && <p>Join Album</p>}
+                    {joining &&
+                        <div role="status">
+                            <svg aria-hidden="true" className="w-4 h-4 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                                <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+                            </svg>
+                            <span className="sr-only">Loading...</span>
+                        </div>
+                    }
+                </button>
+            }
         </div>
     )
 }
