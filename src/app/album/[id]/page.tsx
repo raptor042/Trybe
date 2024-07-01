da9da377155e93f0489ee3c2424ad844f5fe98f4
Benji Chimex <benjichimex@MacBooks-MacBook-Pro.local>
2024-07-01 08:29:08 +0100

    another commit

--- src/app/album/[id]/page.tsx
+++ src/app/album/[id]/page.tsx
@@ -101,7 +101,7 @@ const Page = () => {
                 <main className='flex flex-col items-center w-full m-2'>
                     <Image src={album[6]} alt='' width={300} height={200} />
                     <div className='flex flex-col gap-1 items-center'>
-                        <p className='font-bold text-2xl'>{`Album ${album[0]} - ${album[4]}`}</p>
+                        <p className='font-bold text-2xl'>{album[4]}</p>
                         <p className='text-sm'>{album[5]}</p>
                     </div>
                 </main>
